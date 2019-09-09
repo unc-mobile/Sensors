3f6215bf4c4fda7e321da2f348f60898a05b34c3
Leon Scroggins III <scroggo@google.com>
2019-09-09 09:38:04 -0400

    Listen to a sensor!
    Accelerometer for now

--- app/src/main/java/com/example/scroggo/sensorpractice/MainActivity.java
+++ app/src/main/java/com/example/scroggo/sensorpractice/MainActivity.java
@@ -17,7 +17,12 @@ import org.w3c.dom.Text;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    LinearLayout mLayout;
+    static final String SENSOR_NAME = "sensor";
+    static final String SENSOR_TYPE = "sensor_type";
+    static final String SENSOR_STRING_TYPE = "sensor_string_type";
+
+    private LinearLayout mLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (Sensor sensor : sensors) {
             TextView name = addField(sensor.getName());
             name.setOnClickListener(this);
+            name.setTag(sensor);
 
+            addField("\tTYPE: " + sensor.getStringType());
             float range = sensor.getMaximumRange();
             addField("\tMaximum range: " + range);
             addField("\tResolution: " + sensor.getResolution());
@@ -53,8 +60,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         TextView textView = (TextView) view;
         String name = textView.getText().toString();
+        Sensor sensor = (Sensor) textView.getTag();
+
         Intent intent = new Intent(this, SensorStats.class);
-        intent.putExtra(Intent.EXTRA_TEXT, name);
+        intent.putExtra(SENSOR_NAME, name);
+        intent.putExtra(SENSOR_TYPE, sensor.getType());
+        intent.putExtra(SENSOR_STRING_TYPE, sensor.getStringType());
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/scroggo/sensorpractice/SensorStats.java
+++ app/src/main/java/com/example/scroggo/sensorpractice/SensorStats.java
@@ -7,25 +7,86 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SensorStats extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(Intent.EXTRA_TEXT);
+        String name = intent.getStringExtra(MainActivity.SENSOR_NAME);
         setTitle(name);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        final int invalidSensor = Sensor.TYPE_ALL;
+        int type = intent.getIntExtra(MainActivity.SENSOR_TYPE, invalidSensor);
+        if (type == invalidSensor) {
+            // Hmm, is it safe to call finish from onCreate?
+            errorOut("Calling is missing SENSOR_TYPE!");
+            return;
+        }
+
+        String stringType = intent.getStringExtra(MainActivity.SENSOR_STRING_TYPE);
+        mSensor = mSensorManager.getDefaultSensor(type);
+        if (mSensor == null) {
+            errorOut("Could not find default sensor for " + stringType);
+            return;
+        }
+
+        Toast.makeText(this, "Listening for " + stringType, Toast.LENGTH_SHORT);
+    }
+
+    private void errorOut(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        finish();
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        switch (sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER: {
+                String text = "x: " + sensorEvent.values[0]
+                            + "y: " + sensorEvent.values[1]
+                            + "z: " + sensorEvent.values[2];
+                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default:
+                Toast.makeText(this,
+                        "Other sensor " + sensorEvent.sensor.getStringType(),
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
+    private void log(String msg) {
+        Log.d("SCROGGO", msg);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+        Toast.makeText(this, "onAccuracyChanged: " + i, Toast.LENGTH_SHORT);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mSensor != null) {
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensor != null) {
+            mSensorManager.unregisterListener(this, mSensor);
+        }
     }
 }
